c8c15f6e475f7c5790b87e761c5414b4ae69fd57
Steve R <gitstever@gmail.com>
2016-02-15 17:19:55 +0000

    split javascript into separate file

---
+++ python/static/gopigoflask.js
@@ -0,0 +1,17 @@
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if (request.readyState === 4) {
+        if (request.status === 200) {
+            document.body.className = 'ok';
+            console.log(request.responseText);
+        } else {
+            document.body.className = 'error';
+        }
+    }
+};
+
+function piCommand(command){
+    request.open("GET", command);
+    request.send(null);
+}
+
